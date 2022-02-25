78635d2142fea89de3c464dd9b1bd6ca209cc1be
Anders <anders@andersnoren.se>
2022-02-25 11:36:00 +0100

    Tweaked prev

--- wp-content/themes/lnob/inc/parts/global-goal.php
+++ wp-content/themes/lnob/inc/parts/global-goal.php
@@ -39,9 +39,9 @@ $gg_text_contrast_c = $gg_text_c == 'white' ? 'black' : 'white';
 				<span class="d-tl-none"><?php echo $gg_number; ?></span>
 			</div>
 
-			<div class="gg-hero-content">
+			<div class="gg-hero-content pv-64 pv-t-96">
 
-				<div class="gg-number ff-giorgio-sans d-none d-tl-block c-<?php echo $gg_text_contrast_c; ?>"><?php echo $gg_number; ?></div>
+				<div class="gg-number ff-giorgio-sans pu-t-96 d-none d-tl-block c-<?php echo $gg_text_contrast_c; ?>"><?php echo $gg_number; ?></div>
 
 				<?php if ( $title ) : ?>
 					<h2 class="gg-title ff-giorgio-sans fs-48 fs-t-80 fs-tl-96 no-margin c-<?php echo $gg_text_c; ?>"><?php echo $title; ?></h2>
