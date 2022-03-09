76b2e2a08cd6b7f4b5c42d365172c53afafeacf9
Anders <anders@andersnoren.se>
2022-03-09 16:53:06 +0100

    Fixed comma replacement in numbers block

--- wp-content/themes/lnob/assets/javascript/construct.js
+++ wp-content/themes/lnob/assets/javascript/construct.js
@@ -35,7 +35,7 @@ function getQueryStringValue( key, string ) {
 
 function commaSeparateNumber( val ) {
 	while ( /(\d+)(\d{3})/.test( val.toString() ) ) {
-		val = val.toString().replace(/(\d+)(\d{3})/, '$1' + ',' + '$2');
+		val = val.toString().replace(/(\d+)(\d{3})/, '$1' + ' ' + '$2');
 	}
 	return val;
 }
@@ -1130,7 +1130,7 @@ LNOB.countUp = {
 		$( '.count-up' ).each( function() {
 
 			// Remove thousand decimals, since we add those ourselves on count
-			var countValue = $( this ).text().replace( ',', '' );
+			var countValue = $( this ).text();
 
 			// Skip counters with dot decimals
 			if ( countValue.indexOf( '.' ) >= 0 ) return;
